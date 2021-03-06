19e336b26ab44d77c908d4ded787908af6934d12
arisoftapp <56931409+arisoftapp@users.noreply.github.com>
2021-03-06 12:56:01 -0700

    modificando login

---
+++ src/auth_compras.js
@@ -0,0 +1,47 @@
+﻿const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+config = require('./config');
+
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+const port = process.env.PORT || 3101;
+
+app.set('llave', config.llave);
+app.use(morgan('dev'));
+app.use(cors({ credentials: true, origin: true }));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const rutasProtegidas = express.Router();
+rutasProtegidas.use((req, res, next) => {
+    const token = req.headers['access-token'];
+
+    if (token) {
+        jwt.verify(token, app.get('llave'), (err, decoded) => {
+            if (err) {
+                return res.json({ mensaje: 'Falló la autenticación del token.' });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        res.send({
+            success: true,
+            mensaje: 'Token no proveída.'
+        });
+    }
+});
+
+app.get('/prueb', cors(), (req, res) => {
+    res.send({ mensaje: "hola mundo" })
+})
+require('./rutas/login')(app, rutasProtegidas);
+
+app.listen(port, () => {
+    console.log("apirest " + port);
+})
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,10 +1,55 @@
-const user = require('../models/login');
-module.exports = function(app) {
+const user = require('../modelos/login');
+const jwt = require('jsonwebtoken');
+module.exports = function (app, rutasprotegidas) {
 
-    app.get('/login/:usuario/:contra', (req, res) => {
-        var usuario = req.params.usuario;
-        var contra = req.params.contra;
-        user.getValidarUsuario(usuario, (err, data) => {
+    app.get('/usuarios', rutasprotegidas, (req, res) => {
+        user.getValidarUsuario((err, data) => {
+            if (err) {
+                res.status(500).send({
+                    success: false,
+                    mensaje: 'Error al buscar usuarios:' + err
+                });
+            } else {
+                res.json({
+                    success: true,
+                    respuesta: data,
+                    mensaje: "consulta con exito"
+
+                })
+
+            }
+        });
+    });
+    app.get('/usuario/:usu', (req, res) => {
+        var usu = req.params.usu;
+        user.getUsuario(usu, (err, data) => {
+            if (err) {
+                res.status(500).send({
+                    success: false,
+                    mensaje: 'Error al validar usuario:' + err
+                });
+            } else {
+                if (data.length < 1) {
+                    res.json({
+                        success: false,
+                        mensaje: "usuario incorrecto"
+                    });
+                } else {
+                    res.json({
+                        success: true,
+                        respuesta: data,
+                        mensaje: "consulta con exito"
+
+                    })
+                }
+            }
+        });
+    });
+    app.post('/login', (req, res) => {
+        var usuario = req.body.usuario;
+        var contra = req.body.contra;
+        console.log(req.body);
+        user.auth(usuario, (err, data) => {
             if (err) {
                 res.status(500).send({
                     success: false,
@@ -24,16 +69,65 @@ module.exports = function(app) {
                             mensaje: "contraseña incorrecta"
                         });
                     } else {
-                        res.json({
-                            success: true,
-                            usuario: data,
-                        });
+                        if (data[0].sesion == 1) {
+                            res.json({
+                                success: false,
+                                mensaje: "usuario ya inicio sesion"
+                            });
+                        } else {
+                            var ses = 1;
+                            var usuaux = usuario.toUpperCase();
+                            if (usuaux == "ADMIN") {
+                                ses = 0;
+                            }
+                            user.updateSesion(ses, usuario, (err, dataUpdate) => {
+                                if (err) {
+                                    res.status(500).send({
+                                        success: false,
+                                        message: 'Error al iniciar sesion:' + err
+                                    });
+                                } else {
+                                    const payload = {
+                                        check: true
+                                    };
+                                    const token = jwt.sign(payload, app.get('llave'), {
+
+                                    });
+                                    res.json({
+                                        success: true,
+                                        usuario: data,
+                                        token: token,
+                                        ruta: data[0].ruta,
+                                        info: dataUpdate,
+                                        empresa: data[0].nombre_empresa,
+                                        id_empresa: data[0].idempresa,
+                                        mensaje: "Bienvenido"
+                                    });
+                                }
+                            })
+                        }
                     }
-
                 }
-
             }
         });
+    });
+    app.get('/logout/:usuario', (req, res) => {
+
+        user.updateSesion(0, req.params.usuario, (err, dataUpdate) => {
+            if (err) {
+                res.status(500).send({
+                    success: false,
+                    message: 'Error al cerrar sesion:' + err
+                });
+            } else {
+
+                res.json({
+                    success: true,
+                    info: dataUpdate,
+                    mensaje: "Sesion Cerrada"
+                });
+            }
+        })
     });
 
 }
